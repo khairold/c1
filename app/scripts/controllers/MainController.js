ee755c0aaf786df37cb64f17ed9b9d42058cef90
khairold <khairold@gmail.com>
2015-01-27 18:02:17 +0800

    csp

--- app/scripts/controllers/MainController.js
+++ app/scripts/controllers/MainController.js
@@ -1,6 +1,4 @@
-
-
-app.controller('MainController', function($scope, $timeout, Weather, UserService) {
+app.controller('MainController', function($scope, $http, $timeout, Weather, UserService, instagram) {
   // Build the date object
   $scope.date = {};
   // Update function
@@ -21,6 +19,35 @@ app.controller('MainController', function($scope, $timeout, Weather, UserService
   .then(function(data) {
     $scope.weather.forecast = data;
   });
+
+  $scope.kaka = 'k';
+
+
+  //$http.get('https://rest-service.guides.spring.io/greeting').
+  //$http.get('https://api.instagram.com/v1/tags/sayajual/media/recent?client_id=642176ece1e7445e99244cec26f4de1f&callback=JSON_CALLBACK').
+//  $http.get('https://api.instagram.com/v1/tags/sayajual/media/recent?client_id=642176ece1e7445e99244cec26f4de1f').
+    //    success(function(data) {
+    //        $scope.lala = data;
+    //    });
+
+$scope.pics = [];
+      $scope.have = [];
+      $scope.orderBy = "-likes.count";
+      $scope.getMore = function() {
+        instagram.fetchPopular(function(data) {
+            for(var i=0; i<data.length; i++) {
+              if (typeof $scope.have[data[i].id]==="undefined") {
+                $scope.pics.push(data[i]) ;
+                $scope.have[data[i].id] = "1";
+              }
+            }
+        });
+      };
+      $scope.getMore();
+
+        $scope.tags = [
+            'Bootstrap', 'AngularJS', 'Instagram', 'Factory'
+        ]
 
 
 });
